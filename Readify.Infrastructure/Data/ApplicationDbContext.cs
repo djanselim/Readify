9d9fe5ed595f8a35d439b8b50820e56003ed962c
djanselim <114089140+djanselim@users.noreply.github.com>
2024-04-02 13:12:39 +0300

    Added DbSets

--- Readify.Infrastructure/Data/ApplicationDbContext.cs
+++ Readify.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Readify.Infrastructure.Data.Models;
 
 namespace Readify.Infrastructure.Data
 {
@@ -8,6 +9,27 @@ namespace Readify.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Book>()
+               .HasOne(b => b.Author)
+               .WithMany(a => a.Books)
+               .HasForeignKey(b => b.AuthorId)
+               .IsRequired();
+
+            builder.Entity<Book>()
+                .HasOne(b => b.Category)
+                .WithMany(c => c.Books)
+                .HasForeignKey(b => b.CategoryId)
+                .IsRequired();
+
+            base.OnModelCreating(builder);
         }
     }
 }
