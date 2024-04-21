9a0ede0677c6c5605a5f2b59c667a114fa191087
djanselim <114089140+djanselim@users.noreply.github.com>
2024-04-21 17:43:44 +0300

    Repositories Added

---
+++ Readify.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Readify.Infrastructure.Data.Common
+{
+	public class Repository : IRepository
+	{
+		private readonly DbContext context;
+
+		public Repository(ApplicationDbContext _context)
+		{
+			context = _context;
+		}
+
+		private DbSet<T> DbSet<T>() where T : class
+		{
+			return context.Set<T>();
+		}
+
+		public IQueryable<T> All<T>() where T : class
+		{
+			return DbSet<T>();
+		}
+
+		public IQueryable<T> AllReadOnly<T>() where T : class
+		{
+			return DbSet<T>().AsNoTracking();
+		}
+	}
+}
