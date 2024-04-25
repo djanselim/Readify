e2d212c276ae4a892b0ae7987a7efbd7213b714f
djanselim <114089140+djanselim@users.noreply.github.com>
2024-04-25 16:14:52 +0300

    Principal Extension Added

---
+++ Readify/Extensions/CalimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Readify.Extensions
+{
+    public static class CalimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
